bf0030d41bc5b3ac56866f29483d8a63c73d7b1b
Johnson Pan <panjunsheng@msn.com>
2016-10-11 13:35:59 -0700

    Update create_cert.sh

--- cert/create_cert.sh
+++ cert/create_cert.sh
@@ -4,7 +4,7 @@ sudo apt install -y easy-rsa
 mkdir server
 sudo chmod 0777 server
 cd server
-#openssl dhparam -out dh2048.pem 2048
+openssl dhparam -out dh2048.pem 2048
 openssl genrsa -out server.key 4096
 openssl req -new -out server.csr -key server.key -config ../openssl.cnf -batch
 openssl x509 -req -in server.csr -CA ../ca/ca.crt -CAkey ../ca/ca.key -CAcreateserial -out server.crt -days 3650 -extensions x509_ext -extfile ../openssl.cnf
